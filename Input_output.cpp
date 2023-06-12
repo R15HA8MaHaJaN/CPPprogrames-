24a72d6ff269ed002fe167da8926cf70cfb7d6b5
R15HA8_MaHaJaN <135622641+R15HA8-Mahajan@users.noreply.github.com>
2023-06-12 15:48:48 -0700

    Create Input_output.cpp
    This code is an example of how to take input and print it.

---
+++ Input_output.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+string name;
+
+
+int main(){
+  // we use C - OUT as a predifine output function to print data in c++.
+cout<<"Hello world \nThis is my first code in .cpp"<<endl;
+ 
+  // programe to print name, using user input and output
+cout<<"Write your name-->"<<endl;
+  // we use C - IN function to take input form user.
+  cin>> name ;     // here " name " is a varaible which holdes the data (Your_name) in it.
+cout<< "My name is -"<<  name <<endl;
+  
+  
+return 0;
+}
